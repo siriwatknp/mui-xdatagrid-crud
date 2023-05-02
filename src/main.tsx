555ea96401aa48d32908e1b49901a800ce6aae10
siriwatknp <siriwatkunaporn@gmail.com>
2023-05-01 21:20:56 -0700

    add msw CRUD

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { worker } from "./mocks/browser.ts";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+async function prepare() {
+  if (import.meta.env.DEV) {
+    return worker.start();
+  }
+}
+
+prepare().then(() => {
+  ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+});
